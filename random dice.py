c32f73bba780c5704734e374f17609b4b66b3430
ULTRACODE1521 <99091660+ULTRACODE1521@users.noreply.github.com>
2022-03-06 10:25:51 -0500

    Add files via upload

---
+++ random dice.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import random
+
+ #main window, resize, add title
+root = tk.Tk()
+root.geometry('750x750')
+root.title('Roll Dice')
+
+ #label to display dice
+label = tk.Label(root, text=' ', font=('Helvetica', 260))
+
+
+#function activated by bottom
+def roll_dice():
+     #unicode character strings for dice
+    dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    print(f'{random.choice(dice)} {random.choice(dice)}')
+    label.configure(text=f'{random.choice(dice)} {random.choice(dice)}')
+
+    label.pack()
+
+ #button
+button = tk.Button(root, text='click to roll dice', foreground='red', command=roll_dice)
+
+# pack a widget in the parent widget
+button.pack()
+
+# call the mainloop of Tk
+# keeps window open
+root.mainloop()
